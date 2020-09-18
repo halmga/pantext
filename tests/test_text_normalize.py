f868eef938b32dacb7b72a0222908f3bfb3f3823
halmga <42703971+halmga@users.noreply.github.com>
2020-09-18 15:42:40 +0200

    Update test_text_normalize.py

--- tests/test_text_normalize.py
+++ tests/test_text_normalize.py
@@ -14,16 +14,14 @@ dickens = ["It was the best of times. it was the worst of times!",
 
 
 def test_clean_list_lemmas():
-    assert pantext.text_normalize(dickens, method = 'lemmas') ==
-    [['it','be','the','best','of','time','it','be','the','worst','of','time'],                                 
-     ['it','be','the','age','of','wisdom','it','be','the','age','of','foolishness','it','be','the','epoch','of','belief'],
-     ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
+    assert pantext.text_normalize(dickens, method = 'lemmas') == [['it','be','the','best','of','time','it','be','the','worst','of','time'],
+                                                                  ['it','be','the','age','of','wisdom','it','be','the','age','of','foolishness','it','be','the','epoch','of','belief'],
+                                                                  ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
 
 def test_clean_list_stems():
-    assert pantext.text_normalize(dickens, method = 'stems') ==
-    [['it','wa','the','best','of','time','it','wa','the','worst','of','time'],
-     ['it','wa','the','age','of','wisdom','it','wa','the','age','of','foolish','it','wa','the','epoch','of','belief'],
-     ['it', 'wa', 'the', 'epoch', 'of', 'incredul']]
+    assert pantext.text_normalize(dickens, method = 'stems') == [['it','wa','the','best','of','time','it','wa','the','worst','of','time'],
+                                                                 ['it','wa','the','age','of','wisdom','it','wa','the','age','of','foolish','it','wa','the','epoch','of','belief'],
+                                                                 ['it', 'wa', 'the', 'epoch', 'of', 'incredul']]
     
 def test_nested_list():
     nested = [['too many lists']]
