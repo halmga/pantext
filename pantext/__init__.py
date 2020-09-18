7eb87bdcf1a418101eaec028861364c118627faf
halmga <halm.gabriel@gmail.com>
2020-09-18 15:36:20 +0200

    updates

--- pantext/__init__.py
+++ pantext/__init__.py
@@ -3,7 +3,6 @@
 
 # In[ ]:
 
-# from nltk.corpus import stopwords
+
 from .utils import text_normalize, clean_text, concat_normtext
 from .quantifiers import Quantifiers
-# nltk.download('stopwords')
--- tests/test_text_normalize.py
+++ tests/test_text_normalize.py
@@ -5,7 +5,8 @@
 
 
 import pytest
-from pantext import text_normalize
+import pantext
+
 
 dickens = ["It was the best of times. it was the worst of times!",
            "it was the age of wisdom. It was the age of foolishness. it was the epoch of belief?",
@@ -13,17 +14,21 @@ dickens = ["It was the best of times. it was the worst of times!",
 
 
 def test_clean_list_lemmas():
-    assert text_normalize(dickens, method = 'lemmas') == [['it','be','the','best','of','time','it','be','the','worst','of','time'],
-                                       ['it','be','the','age','of','wisdom','it','be','the','age','of','foolishness','it','be','the','epoch','of','belief'],
-                                       ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
+    assert pantext.text_normalize(dickens, method = 'lemmas') ==
+    [['it','be','the','best','of','time','it','be','the','worst','of','time'],                                 
+     ['it','be','the','age','of','wisdom','it','be','the','age','of','foolishness','it','be','the','epoch','of','belief'],
+     ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
+
 def test_clean_list_stems():
-    assert text_normalize(dickens, method = 'stems') == [['it','wa','the','best','of','time','it','wa','the','worst','of','time'],
-                                                         ['it','wa','the','age','of','wisdom','it','wa','the','age','of','foolish','it','wa','the','epoch','of','belief'],
-                                                         ['it', 'wa', 'the', 'epoch', 'of', 'incredul']]
+    assert pantext.text_normalize(dickens, method = 'stems') ==
+    [['it','wa','the','best','of','time','it','wa','the','worst','of','time'],
+     ['it','wa','the','age','of','wisdom','it','wa','the','age','of','foolish','it','wa','the','epoch','of','belief'],
+     ['it', 'wa', 'the', 'epoch', 'of', 'incredul']]
+    
 def test_nested_list():
     nested = [['too many lists']]
     with pytest.raises(TypeError):
-        text_normalize(nested)
+        pantext.text_normalize(nested)
     
 
 
