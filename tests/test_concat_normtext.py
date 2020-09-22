76a56b59109259bfa8e3915a951308f683f82a34
halmga <halm.gabriel@gmail.com>
2020-09-22 11:28:18 +0200

    Update tests

--- tests/test_concat_normtext.py
+++ tests/test_concat_normtext.py
@@ -14,14 +14,12 @@ dickens = [['it','be','the','best','of','time','it','be','the','worst','of','tim
 def test_normal_concat():
     assert pantext.concat_normtext(dickens)== [' it be the best of time it be the worst of time',' it be the age of wisdom it be the age of foolishness it be the epoch of belief',' it be the epoch of incredulity']
     
-dickens_missing_first = ["",
-           "it was the age of wisdom. It was the age of foolishness. it was the epoch of belief?",
-           "IT WAS THE EPOCH OF INCREDULITY!?#99"]
+dickens_missing_first = [[], ['it', 'be', 'the', 'age', 'of', 'wisdom', 'it', 'be', 'the', 'age', 'of', 'foolishness', 'it', 'be', 'the', 'epoch', 'of', 'belief'], ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
 
 def test_concat_first_missing():
     assert pantext.concat_normtext(dickens_missing_first) == ['', ' it be the age of wisdom it be the age of foolishness it be the epoch of belief', ' it be the epoch of incredulity']
 
-dickens_missing = ["It was the best of times. it was the worst of times!","","IT WAS THE EPOCH OF INCREDULITY!?#99"]
+dickens_missing = [['it', 'be', 'the', 'best', 'of', 'time', 'it', 'be', 'the', 'worst', 'of', 'time'], [], ['it', 'be', 'the', 'epoch', 'of', 'incredulity']]
 
 def test_concat_missing():
     assert pantext.concat_normtext(dickens_missing) == [' it be the best of time it be the worst of time', '', ' it be the epoch of incredulity']
--- tests/test_quantifier.py
+++ tests/test_quantifier.py
@@ -10,6 +10,7 @@ import pantext
 dickens = ["It was the best of times. it was the worst of times!",
            "it was the age of wisdom. It was the age of foolishness. it was the epoch of belief?",
            "IT WAS THE EPOCH OF INCREDULITY!?#99"]
+
 Q = pantext.Quantifier(dickens)
 
 def test_normal_sentcount():
@@ -23,8 +24,8 @@ def test_english_stopcount():
     
 dickens_missing = ["It was the best of times. it was the worst of times!","","IT WAS THE EPOCH OF INCREDULITY!?#99"]
 
-Q = pantext.Quantifier(dickens_missing)
+Q_missing = pantext.Quantifier(dickens_missing)
 
 def test_zero_percent():
-    assert Q.stopword_percent(stop_words="english") == [0.57, 0.0, 0.4]
+    assert Q_missing.stopword_percent(stop_words="english") == [0.57, 0.0, 0.4]
 
