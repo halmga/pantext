edbd89e7a11bfefe1b6dbfb0e52b5cbf30c29e2f
halmga <42703971+halmga@users.noreply.github.com>
2020-09-22 12:20:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,13 @@ setup(name='pantex',
       download_url = 'https://github.com/halmga/pantext/archive/0.0.1.tar.gz',
       author='halmga',
       license='MIT',
-      install_requires= ['pandas','nltk','tqdm'])
+      install_requires= ['pandas','nltk','tqdm']
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', # Your License Here  
+        'Programming Language :: Python :: 3',    # List Python versions that you support Here  
+        'Programming Language :: Python :: 3.4',
+        ],
+)
