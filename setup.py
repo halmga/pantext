6ed8a9df78ce02ed083298f76e7fbecb0e669c11
halmga <42703971+halmga@users.noreply.github.com>
2020-09-22 11:53:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,12 @@
 
 from setuptools import setup
 
-# setup(name= 'pantex',
-#      version= '0.0.1',
-#      description= 'text cleaning package for use on pandas dataframes and lists',
-#      author= 'Gabriel Halm',
-#      packages= ['pantex'],
-#      install_requires= ['pandas','numpy','nltk','tqdm'])
-
-# from distutils.core import setup
-
 setup(name='pantex',
       packages = ['pantex'],
       version= '0.0.1',
       description= 'text cleaning package for use on pandas dataframes and lists,
       url='https://github.com/halmga/pantext',
-      download_url = 'https://github.com/halmga/pantext/archive/0.0.1.tar.gz', #FILL IN LATER
+      download_url = 'https://github.com/halmga/pantext/archive/0.0.1.tar.gz',
       author='halmga',
       license='MIT',
       install_requires= ['pandas','nltk','tqdm'])
